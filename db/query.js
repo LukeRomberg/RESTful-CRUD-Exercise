253b28c9a1d78a1356aff3e0c895fcad0d96196c
Luke Romberg <Lukerromberg@gmail.com>
2017-10-19 23:36:54 -0600

    added query

--- db/query.js
+++ db/query.js
@@ -1,29 +1,29 @@
 var db = require('./connection');
-
-function getAll() {
-	// Get all cartoons and return them to the route
-}
-
-function getOne(id) {
-	// Get one cartoon by the id and return it
-}
-
-function create(cartoon) {
-	// Add a cartoon to the database
-}
-
-function update(id, cartoon) {
-	// Update a cartoon with the specified id
-}
-
-function remove(id) {
-	// Delete the specified cartoon
-}
-
+const knex = require('knex');
+const config = require('../knexfile.js');
+const pg = knex(config.development)
 module.exports = {
-	getAll,
-	getOne,
-	create,
-	update,
-	remove
+  getAll: function() {
+    // Get all cartoons and return them to the route
+    return pg('cartoon').select('*')
+  },
+
+  getOne: function(id) {
+    // Get one cartoon by the id and return it
+    return pg('cartoon').select('*').where('id', id)
+  },
+
+  create: function(cartoon) {
+    // Add a cartoon to the database
+    return pg('cartoon').insert(cartoon).returning('*')
+  },
+
+  update: function(id, cartoon) {
+    // Update a cartoon with the specified id
+  },
+
+  remove: function(id) {
+    // Delete the specified cartoon
+  },
+
 };
